a62eac0ac271963e05e817b8f93fabbd45315a12
Girish Palya <girishji@gmail.com>
2022-08-05 17:31:42 +0200

    Add a new keyboard for Kailh Choc switches
    Full PCB and top plate for the keyboard is in
    https://github.com/girishji/choc-keyboard
    
    A  keyboards/chockb/rev1/blackpill_f401.c
    A  keyboards/chockb/rev1/chconf.h
    A  keyboards/chockb/rev1/config.h
    A  keyboards/chockb/rev1/halconf.h
    A  keyboards/chockb/rev1/info.json
    A  keyboards/chockb/rev1/keymaps/default/keymap.c
    A  keyboards/chockb/rev1/keymaps/default/readme.md
    A  keyboards/chockb/rev1/mcuconf.h
    A  keyboards/chockb/rev1/readme.md
    A  keyboards/chockb/rev1/rev1.c
    A  keyboards/chockb/rev1/rev1.h
    A  keyboards/chockb/rev1/rules.mk

---
+++ keyboards/chockb/rev1/keymaps/default/keymap.c
@@ -0,0 +1,156 @@
+// Copyright 2022 Girish Palya (@girishji)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// see https://github.com/qmk/qmk_firmware/blob/e1203a222bb12ab9733916164a000ef3ac48da93/keyboards/alu84/keymaps/default/keymap.c
+// 
+
+#include QMK_KEYBOARD_H
+
+bool is_cmd_grv_active = false; 
+uint16_t cmd_grv_timer = 0;    
+
+bool is_cmd_tab_active = false; 
+uint16_t cmd_tab_timer = 0;    
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  CMD_GRV = SAFE_RANGE,
+  CMD_TAB
+};
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Base */
+  [_BASE] = LAYOUT(
+    CMD_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_PGUP,
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN,
+    OSM(MOD_LCTL), KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_RIGHT,
+    OSM(MOD_LSFT), DYN_MACRO_PLAY1, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_UP, OSM(MOD_RALT), 
+    OSM(MOD_LALT), KC_BTN1, KC_BTN3, KC_CAPS, OSM(MOD_LGUI), KC_BSPC, OSL(_FN), KC_SPC, KC_ENT, OSM(MOD_RGUI), LGUI(KC_LEFT), LGUI(KC_RIGHT), KC_LEFT, KC_DOWN, KC_RIGHT 
+  ),
+  [_FN] = LAYOUT(
+    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_HOME, KC_END,
+    _______, DYN_REC_STOP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, DYN_REC_START1, DYN_MACRO_PLAY1, _______, _______, _______, LCMD(KC_LEFT), KC_PGDN, KC_PGUP, LCMD(KC_RIGHT), _______, _______, _______, _______,
+    _______, DYN_REC_START1, _______, _______, _______, _______, DYN_REC_START2, DYN_MACRO_PLAY2, _______, _______, _______, _______, _______, KC_WH_U, _______,
+    RESET, KC_BTN2, _______, _______, _______, BL_TOGG, _______, _______, _______, _______, _______, _______, BL_DEC, KC_WH_D, BL_INC
+  )
+};
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  // debug_enable=true;
+  // debug_matrix=true;
+  // debug_keyboard=true;
+  ////debug_mouse=true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %u, time: %u, interrupt: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+
+  switch (keycode) {
+    case CMD_GRV:
+      if (record->event.pressed) {
+        if (!is_cmd_grv_active) {
+          is_cmd_grv_active = true;
+          register_code(KC_LGUI);
+        }
+        cmd_grv_timer = timer_read(); 
+        register_code(KC_GRV);
+      } else {
+        unregister_code(KC_GRV);
+      }
+      return true;
+    case CMD_TAB:
+      if (record->event.pressed) {
+        if (!is_cmd_tab_active) {
+          is_cmd_tab_active = true;
+          register_code(KC_LGUI);
+        }
+        cmd_tab_timer = timer_read(); 
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      return true;
+    default:
+      return true; // Process all other keycodes normally
+  }
+
+}
+
+void matrix_scan_user(void) { // The very important timer.  
+  if (is_cmd_grv_active) {    
+    if (timer_elapsed(cmd_grv_timer) > 700) {      
+      unregister_code(KC_LGUI);      
+      is_cmd_grv_active = false;    
+    }  
+  } else if (is_cmd_tab_active) {    
+    if (timer_elapsed(cmd_tab_timer) > 700) {      
+      unregister_code(KC_LGUI);      
+      is_cmd_tab_active = false;    
+    }  
+  }
+}
+
+/* Enable some leds when capslock is on */
+void led_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+  if (host_keyboard_led_state().caps_lock) {
+        for (uint8_t i = led_min; i <= led_max; i++) {
+            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
+                led_matrix_set_value(i, 0xc0);
+            }
+        }
+    }
+}
+
+#if 1
+const is31_led PROGMEM g_is31_leds[DRIVER_LED_TOTAL] = {
+/* Refer to IS31 manual for these locations
+ *    driver
+ *    |  LED address
+ *    |  | */
+    { 0, C1_1  }, { 0, C1_2  }, { 0, C1_3  }, { 0, C1_4  }, { 0, C1_5  }, { 0, C1_6  }, { 0, C1_7  }, { 0, C1_8  },
+    { 0, C2_1  }, { 0, C2_2  }, { 0, C2_3  }, { 0, C2_4  }, { 0, C2_5  }, { 0, C2_6  }, { 0, C2_7  }, { 0, C2_8  },
+    { 0, C3_1  }, { 0, C3_2  }, { 0, C3_3  }, { 0, C3_4  }, { 0, C3_5  }, { 0, C3_6  }, { 0, C3_7  }, { 0, C3_8  },
+    { 0, C4_1  }, { 0, C4_2  }, { 0, C4_3  }, { 0, C4_4  }, { 0, C4_5  }, { 0, C4_6  }, { 0, C4_7  }, { 0, C4_8  },
+    { 0, C5_1  }, { 0, C5_2  }, { 0, C5_3  }, { 0, C5_4  }, { 0, C5_5  }, { 0, C5_6  }, { 0, C5_7  }, { 0, C5_8  },
+    { 0, C6_1  }, { 0, C6_2  }, { 0, C6_3  }, { 0, C6_4  }, { 0, C6_5  }, { 0, C6_6  }, { 0, C6_7  }, { 0, C6_8  },
+    { 0, C7_1  }, { 0, C7_2  }, { 0, C7_3  }, { 0, C7_4  }, { 0, C7_5  }, { 0, C7_6  }, { 0, C7_7  }, { 0, C7_8  },
+    { 0, C8_1  }, { 0, C8_2  }, { 0, C8_3  }, { 0, C8_4  }, { 0, C8_5  }, { 0, C8_6  }, { 0, C8_7  }, { 0, C8_8  },
+    { 0, C9_1  }, { 0, C9_2  }, { 0, C9_3  }, { 0, C9_4  }, { 0, C9_5  }, { 0, C9_6  }, { 0, C9_7  }, { 0, C9_8  },
+};
+
+led_config_t g_led_config = { {
+  // Key Matrix to LED Index
+  { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14 },
+  { NO_LED, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28},
+  { 29, NO_LED, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, NO_LED, 41},
+  { 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56},
+  { 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71},
+}, {
+  // LED Index to Physical Position
+  {   0,   0 }, {  16,   0 }, {  32,   0 }, {  48,   0 }, {  64,   0 }, {  80,   0 }, {  96,   0 }, { 112,   0 }, { 128,   0 }, { 144,   0 }, { 160,   0 }, { 176,   0 }, { 192,   0 }, { 208,   0 }, { 224,   0 }, 
+  {   16,  12 }, {  32,  12 }, {  48,  12 }, {  64,  12 }, {  80,  12 }, {  96,  12 }, { 112,  12 }, { 128,  12 }, { 144,  12 }, { 160,  12 }, { 176,  12 }, { 192,  12 }, { 208,  12 }, { 224,  12 }, 
+  {   0,  24 }, {  32,  24 }, {  48,  24 }, {  64,  24 }, {  80,  24 }, {  96,  24 }, { 112,  24 }, { 128,  24 }, { 144,  24 }, { 160,  24 }, { 176,  24 }, { 192,  24 }, { 224,  24 }, 
+  {   0,  36 }, {  16,  36 }, {  32,  36 }, {  48,  36 }, {  64,  36 }, {  80,  36 }, {  96,  36 }, { 112,  36 }, { 128,  36 }, { 144,  36 }, { 160,  36 }, { 176,  36 }, { 192,  36 }, {208,  36}, { 224,  36 }, 
+  {   0,  48 }, {  16,  48 }, {  32,  48 }, {  48,  48 }, {  64,  48 }, {  80,  48 }, {  96,  48 }, { 112,  48 }, { 128,  48 }, { 144,  48 }, { 160,  48 }, { 176,  48 }, { 192,  48 }, {208,  48}, { 224,  48 }, 
+}, {
+  // LED Index to Flag
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+} };
+
+#endif
